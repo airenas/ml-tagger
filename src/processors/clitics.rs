512eba35ec9affdecd9f1f8a5b6104117e6b4d49
Airenas Vaiciunas <airenass@gmail.com>
2024-04-12 13:23:32 +0300

    feat: add clitics parse tests

--- src/processors/clitics.rs
+++ src/processors/clitics.rs
@@ -60,12 +60,75 @@ fn read_clitics_file(filename: &str) -> anyhow::Result<HashMap<String, Vec<Strin
 fn parse_line(line: String) -> anyhow::Result<(String, Vec<String>)> {
     let parts: Vec<&str> = line.split('\t').collect();
     if let Some(p) = parts.get(1) {
-        let mis: Vec<String> = p.split(';').map(|f| f.split(":").collect()).collect();
+        let mis = p
+            .split(';')
+            .map(|f| f.split(":").skip(1).take(1).collect())
+            .collect::<Vec<String>>();
         let res: Vec<String> = mis.into_iter().filter(|f| f.len() > 0).collect();
         if res.len() == 0 {
             return Err(anyhow::anyhow!("failed parse line: {}", line));
         }
-        return Ok((parts.get(0).unwrap().to_string(), res));
+        let w = parts.get(0).unwrap().trim().to_string();
+        if w.len() == 0 {
+            return Err(anyhow::anyhow!("failed parse line: {}: no  word", line));
+        }
+        return Ok((w, res));
     }
     return Err(anyhow::anyhow!("failed parse line: {}", line));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    fn test_parse(input: String, expected: (String, Vec<String>)) {
+        assert_eq!(parse_line(input).unwrap(), expected);
+    }
+
+    macro_rules! parse_line_test {
+        ($suite:ident, $($name:ident: $input:expr, $expected:expr,)*) => {
+            mod $suite {
+                use super::*;
+                $(
+                    #[test]
+                    fn $name() {
+                        test_parse($input, $expected);
+                    }
+                )*
+            }
+        }
+    }
+
+    parse_line_test!(parse_line_ok,
+        one_mi: "olia ol\t:aaa".to_string(), ("olia ol".to_string(), vec!["aaa".to_string()]),
+        two_mi: "olia ol\taaa:Pgfsdn;aaa:Pgmsdn".to_string(), ("olia ol".to_string(), vec!["Pgfsdn".to_string(), "Pgmsdn".to_string()]),
+        more_mi: "olia more \t:11;:22;:33;:44".to_string(), ("olia more".to_string(), vec!["11".to_string(), "22".to_string(), "33".to_string(), "44".to_string()]),
+    );
+
+    fn test_parse_fail(input: String) {
+        let res = parse_line(input);
+        assert!(res.is_err());
+    }
+
+    macro_rules! parse_line_err_test {
+        ($suite:ident, $($name:ident: $input:expr,)*) => {
+            mod $suite {
+                use super::*;
+                $(
+                    #[test]
+                    fn $name() {
+                        test_parse_fail($input);
+                    }
+                )*
+            }
+        }
+    }
+
+    parse_line_err_test!(parse_line_fail,
+        no_line: "".to_string(),
+        no_word: "\t:olia".to_string(),
+        no_mi: "olia\t".to_string(),
+        no_tab: "olia mi".to_string(),
+    );
 }
